46abfe1d37da0f43df5e615d3c8d367c4f64d520
Romain Dorgueil <romain@dorgueil.net>
2016-04-13 22:15:44 +0200

    remove sphinx module not yet supported by rtfd

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,6 @@ import os
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
